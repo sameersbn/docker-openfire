2004d3770a6257f76f2e573ed0e4c0fe3c1b28fc
Sameer Naik <sameer@damagehead.com>
2015-07-26 19:59:03 +0530

    entrypoint.sh: removed `openfire` sub-directory from data volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,27 +2,34 @@
 set -e
 
 # create openfire data dir
-mkdir -p ${OPENFIRE_DATA_DIR}/openfire
-chmod -R 0755 ${OPENFIRE_DATA_DIR}/openfire
-chown -R ${OPENFIRE_USER}:${OPENFIRE_USER} ${OPENFIRE_DATA_DIR}/openfire
+mkdir -p ${OPENFIRE_DATA_DIR}
+chmod -R 0755 ${OPENFIRE_DATA_DIR}
+chown -R ${OPENFIRE_USER}:${OPENFIRE_USER} ${OPENFIRE_DATA_DIR}
 
 # create openfire log dir
 mkdir -p ${OPENFIRE_LOG_DIR}
 chmod -R 0755 ${OPENFIRE_LOG_DIR}
 chown -R ${OPENFIRE_USER}:${OPENFIRE_USER} ${OPENFIRE_LOG_DIR}
 
+# migrate old directory structure
+if [ -d ${OPENFIRE_DATA_DIR}/openfire ]; then
+  mv ${OPENFIRE_DATA_DIR}/openfire/etc ${OPENFIRE_DATA_DIR}/etc
+  mv ${OPENFIRE_DATA_DIR}/openfire/lib ${OPENFIRE_DATA_DIR}/lib
+  rm -rf ${OPENFIRE_DATA_DIR}/openfire
+fi
+
 # populate default openfire configuration if it does not exist
-if [ ! -d ${OPENFIRE_DATA_DIR}/openfire/etc ]; then
-  mv /etc/openfire ${OPENFIRE_DATA_DIR}/openfire/etc
+if [ ! -d ${OPENFIRE_DATA_DIR}/etc ]; then
+  mv /etc/openfire ${OPENFIRE_DATA_DIR}/etc
 fi
 rm -rf /etc/openfire
-ln -sf ${OPENFIRE_DATA_DIR}/openfire/etc /etc/openfire
+ln -sf ${OPENFIRE_DATA_DIR}/etc /etc/openfire
 
-if [ ! -d ${OPENFIRE_DATA_DIR}/openfire/lib ]; then
-  mv /var/lib/openfire ${OPENFIRE_DATA_DIR}/openfire/lib
+if [ ! -d ${OPENFIRE_DATA_DIR}/lib ]; then
+  mv /var/lib/openfire ${OPENFIRE_DATA_DIR}/lib
 fi
 rm -rf /var/lib/openfire
-ln -sf ${OPENFIRE_DATA_DIR}/openfire/lib /var/lib/openfire
+ln -sf ${OPENFIRE_DATA_DIR}/lib /var/lib/openfire
 
 # allow arguments to be passed to openfire launch
 if [[ ${1:0:1} = '-' ]]; then
